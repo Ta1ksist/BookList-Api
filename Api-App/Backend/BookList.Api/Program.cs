59856311dcd4bdffce6559b84de98aacedb76b51
Izzat_Melikov <gagarin.yurick@mail.ru>
2024-12-04 21:52:50 +0300

    Added Contracts and added BookResponse

--- Api-App/Backend/BookList.Api/Program.cs
+++ Api-App/Backend/BookList.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<BookContext>(options =>
     options.UseNpgsql("Host=localhost;Port=5432;Database=Book;Username=postgres;Password=postgres"));
 
--- Api-App/Backend/BookList.Api/obj/Debug/net8.0/BookList.Api.AssemblyInfo.cs
+++ Api-App/Backend/BookList.Api/obj/Debug/net8.0/BookList.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookList.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4dbbdd236d2866a850861aa23075a7efba6c127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb812ab2233b441d05003cdaa511b4a221b293c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookList.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookList.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
